9d16bb9004b89e85600055a45f8ae797f75bb328
Dhirajmujmule91 <137919560+Dhirajmujmule91@users.noreply.github.com>
2026-08-15 00:09:14 +0530

    Create practical1.py

---
+++ Chapter2/practical1.py
@@ -0,0 +1,7 @@
+# write a program to take age as input and print entered value and its datatype
+
+
+age=input("Enter your age:")
+
+print("Your age is: ", age)
+print("Its datatype is: ", type(age))
